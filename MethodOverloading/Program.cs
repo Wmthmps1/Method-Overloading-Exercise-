7ca428a6df4d9c2d86d5b839b06cd4481d6aec59
William Thompson <wmthmps1@gmail.com>
2023-12-14 16:06:09 -0600

    Initial Commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,42 @@
 {
     public class Program
     {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal a, decimal b)
+        {
+            return a + b;
+        }
+
+        public static string Add(int a, int b, bool isTrue)
+        {
+            int sum = a + b;
+
+            if (!isTrue)
+            {
+                return "Boolean value is false";
+            }
+
+            if(sum == 1)
+            {
+                return $"{sum} dollar";
+            } else
+            {
+                return $"{sum} dollars";
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(5,2));
+            Console.WriteLine(Add(5.3M, 2.8M));
+            Console.WriteLine(Add(1, 0, true));
+            Console.WriteLine(Add(1, 6, true));
+            Console.WriteLine(Add(1, 7, false));
         }
     }
 }
